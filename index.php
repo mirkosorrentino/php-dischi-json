fbe067241cf44e76242f8f895580e77a8b80bf82
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-05-08 16:26:19 +0200

    Project scaffolding

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi</title>
+    <!-- custom CSS style -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- Vue CDN -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+<body>
+    <div id="app">
+       <div class="text-center">Ciao amigo</div>
+    </div>
+    <!-- Custom JS script -->
+    <script src="js/script.js"></script>
+</body>
+</html>
